d2418825de5520da482cd5092483bc4a6e9eb353
juandavidcowboy88 <ing.juandavid.alvarez@gmail.com>
2017-03-17 14:48:10 -0500

    segundo reto listo

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,8 +1,28 @@
 class ActorsController < ApplicationController
 
-def index
-  @actors= Actor.all
-end
+    def index
+      @actors= Actor.all
+    end
 
+    def new
+      @actors = Actor.new
+      end
+
+      def create
+          @actors = Actor.new(actor_params)
+          if @actors.save
+            redirect_to  actors_path , notice: "el actor ha sido creado exitosamente"
+          else
+            render :new
+          end
+
+      end
+
+
+
+    private
+    def actor_params
+    params.require(:actor).permit(:name, :bio, :birth_place, :birth_date, :image_url, :alive, :death_date, :death_place )
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
 get '/actors'  => 'actors#index'
+get '/actors/new' => 'actors#new'
+post '/actors' => 'actors#create'
+
+
+#resources 'actors'
+
+# resources :actors , only [:index, :new ,:create ]
+#get '/create_actor' => 'actors#create'
+#post '/actors' => 'actors#index'
+
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
